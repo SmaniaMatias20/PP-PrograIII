ff16812d7528f1f3b7c291286454bbcf311eff30
Matias <matiasezequielsmania@gmail.com>
2024-10-05 09:14:55 -0300

    rutas

--- build/js/conjunto.js
+++ build/js/conjunto.js
@@ -40,7 +40,7 @@ const articulos = [
     fecha: '10/02/2024',
     autor: 'Tanoni Matias',
     contenido: 'Descubre cómo aprovechar cada rincón de tu casa con ingeniosas soluciones de almacenamiento y diseño. Desde estanterías flotantes hasta muebles multifuncionales, aprende a maximizar el espacio sin sacrificar estilo. Con estas ideas, transformarás tu hogar en un lugar ordenado y acogedor, perfecto para vivir y recibir visitas.',
-    imagen: 'build/img/blog1.jpg',
+    imagen: '../img/blog1.jpg',
   },
   {
     id: 2,
@@ -48,7 +48,7 @@ const articulos = [
     fecha: '25/03/2024',
     autor: 'Palopolo Lujan',
     contenido: 'Te mostramos los colores que estarán en auge este año y cómo puedes incorporarlos en la decoración de tu hogar. Desde tonos cálidos y acogedores hasta colores fríos que aportan serenidad, descubre cómo elegir la paleta adecuada para cada espacio. Aprende a combinar estos colores para crear ambientes armónicos y llenos de personalidad.',
-    imagen: 'build/img/blog2.jpg',
+    imagen: '../img/blog2.jpg',
   },
   {
     id: 3,
@@ -56,7 +56,7 @@ const articulos = [
     fecha: '12/07/2024',
     autor: 'Smania Matias',
     contenido: 'Aprende a diseñar y mantener un pequeño jardín en el interior de tu hogar. Te enseñaremos sobre las plantas más adecuadas para espacios reducidos, el cuidado que requieren y consejos para la disposición ideal. Desde jardines verticales hasta rincones verdes en tus habitaciones, transforma tu hogar en un oasis natural que te conecte con la naturaleza.',
-    imagen: 'build/img/blog3.jpg',
+    imagen: '../img/blog3.jpg',
   },
   {
     id: 4,
@@ -64,7 +64,7 @@ const articulos = [
     fecha: '03/10/2024',
     autor: 'Peña Enzo',
     contenido: 'Encuentra los muebles ideales para tu sala de estar con nuestros consejos prácticos. Descubre qué estilos se adaptan mejor a tu espacio y cómo seleccionar muebles que sean funcionales y estéticamente agradables. Además, te daremos tips sobre la distribución del mobiliario para optimizar el espacio y crear un ambiente acogedor para ti y tus invitados.',
-    imagen: 'build/img/blog4.jpg',
+    imagen: '../img/blog4.jpg',
   }
 ];
 
@@ -72,7 +72,7 @@ const articulos = [
 const propiedades = {
   1: {
     titulo: "Casa de lujo",
-    imagen: "build/img/anuncio1.jpg",
+    imagen: "../img/anuncio1.jpg",
     descripcion: "Descubre esta impresionante casa de lujo con vistas espectaculares al lago. Esta propiedad destaca por sus acabados de alta calidad, que incluyen mármol en los pisos y madera noble en las puertas. La amplia sala de estar se abre a un espacioso patio, ideal para entretenerse, donde podrás disfrutar de atardeceres inolvidables. La cocina, equipada con electrodomésticos de última generación, es un sueño para cualquier chef. Con cuatro dormitorios, cada uno con su propio baño, esta casa es perfecta para familias que buscan comodidad y elegancia en un solo lugar. Su ubicación privilegiada, rodeada de naturaleza, ofrece la tranquilidad que necesitas.",
     precio: "$3,000,000",
     sanitarios: 3,
@@ -81,7 +81,7 @@ const propiedades = {
   },
   2: {
     titulo: "Casa con balcón",
-    imagen: "build/img/anuncio2.jpg",
+    imagen: "../img/anuncio2.jpg",
     descripcion: "Vive en esta moderna casa que combina estilo y funcionalidad. Equipado con tecnología inteligente y completamente amueblado, este hogar ofrece un ambiente acogedor desde el momento en que entras. Los grandes ventanales permiten la entrada de luz natural y ofrecen vistas panorámicas del jardín, que está diseñado con paisajismo profesional. La sala de estar, amplia y luminosa, es perfecta para reuniones familiares, mientras que la cocina gourmet, con una isla central y electrodomésticos de acero inoxidable, invita a la creatividad culinaria. Disfruta de la comodidad de tres dormitorios, cada uno con armarios empotrados y acabados de lujo. Esta casa es el espacio perfecto para disfrutar de la comodidad y el lujo en cada rincón.",
     precio: "$2,000,000",
     sanitarios: 3,
@@ -90,7 +90,7 @@ const propiedades = {
   },
   3: {
     titulo: "Casa de verano",
-    imagen: "build/img/anuncio3.jpg",
+    imagen: "../img/anuncio3.jpg",
     descripcion: "Esta moderna casa, con acceso privado a la playa, es el refugio perfecto para disfrutar de días soleados y la brisa marina. Su diseño elegante combina líneas limpias y espacios abiertos, creando un ambiente relajante. Con tres dormitorios, cada uno decorado con un estilo costero, la casa puede alojar cómodamente a toda la familia. El área al aire libre incluye una terraza espaciosa y una piscina, ideal para refrescarse después de un día en la playa. La cocina, equipada con electrodomésticos de última generación, se conecta a la sala de estar, donde podrás disfrutar de momentos inolvidables con amigos y familiares. Cada rincón de esta casa está diseñado para brindar confort y una conexión con la belleza natural que la rodea.",
     precio: "$4,500,000",
     sanitarios: 2,
@@ -99,7 +99,7 @@ const propiedades = {
   },
   4: {
     titulo: "Casa moderna ",
-    imagen: "build/img/anuncio4.jpg",
+    imagen: "../img/anuncio4.jpg",
     descripcion: "Sumérgete en la vida urbana con esta moderna casa que cuenta con un diseño contemporáneo y tecnología de domótica. Esta propiedad no solo ofrece un estilo de vida lujoso, sino que también proporciona comodidad con su sistema inteligente que controla la iluminación, la temperatura y la seguridad desde tu smartphone. El amplio jardín es un oasis en medio de la ciudad, ideal para disfrutar de barbacoas y momentos de relajación. Con cinco dormitorios, esta casa es perfecta para familias grandes o aquellos que buscan espacio adicional para oficinas o habitaciones de invitados. La cocina de concepto abierto se integra con la sala de estar, creando un ambiente perfecto para socializar y disfrutar de la vida moderna.",
     precio: "$4,200,000",
     sanitarios: 4,
@@ -108,7 +108,7 @@ const propiedades = {
   },
   5: {
     titulo: "Casa minimalista ",
-    imagen: "build/img/anuncio5.jpg",
+    imagen: "../img/anuncio5.jpg",
     descripcion: "Disfruta de la tranquilidad en esta propiedad minimalista con vistas al lago. Con un diseño arquitectónico limpio y contemporáneo, esta casa está diseñada para maximizar el espacio y la luz natural. Los acabados en tonos neutros y materiales naturales crean un ambiente sereno que invita a la relajación. La casa cuenta con tres dormitorios, perfectos para descansar después de un día explorando la naturaleza. La cocina está equipada con electrodomésticos de alta gama y una barra desayunadora, ideal para comidas informales. Además, la sala de estar se abre a un patio, donde podrás disfrutar de la paz que brinda la vista al lago y la belleza del entorno natural.",
     precio: "$3,800,000",
     sanitarios: 3,
@@ -117,7 +117,7 @@ const propiedades = {
   },
   6: {
     titulo: "Casa con alberca",
-    imagen: "build/img/anuncio6.jpg",
+    imagen: "../img/anuncio6.jpg",
     descripcion: "Aprovecha esta excelente oportunidad de adquirir una casa con alberca y acabados de lujo en la ciudad. La piscina es el lugar perfecto para disfrutar de momentos memorables con amigos y familiares en un entorno cómodo y sofisticado. Esta casa cuenta con un diseño abierto que permite que la luz natural fluya por todos los espacios. Con cuatro dormitorios, cada uno con armarios empotrados y ventanales, esta propiedad ofrece tanto comodidad como estilo. La cocina de concepto abierto está conectada a la sala de estar y al comedor, haciendo de este un espacio ideal para la vida cotidiana y el entretenimiento. Además, su ubicación te brinda fácil acceso a los principales servicios y atracciones de la ciudad.",
     precio: "$3,000,000",
     sanitarios: 3,
@@ -164,15 +164,15 @@ function cargarPropiedades(limite = Object.keys(propiedades).length) {
         <p class="precio">${propiedad.precio}</p>
         <ul class="iconos-caracteristicas">
           <li>
-            <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono_wc" />
+            <img class="icono" loading="lazy" src="../img/icono_wc.svg" alt="icono_wc" />
             <p>${propiedad.sanitarios}</p>
           </li>
           <li>
-            <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono_estacionamiento" />
+            <img class="icono" loading="lazy" src="../img/icono_estacionamiento.svg" alt="icono_estacionamiento" />
             <p>${propiedad.estacionamientos}</p>
           </li>
           <li>
-            <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono_dormitorio" />
+            <img class="icono" loading="lazy" src="../img/icono_dormitorio.svg" alt="icono_dormitorio" />
             <p>${propiedad.dormitorios}</p>
           </li>
         </ul>
